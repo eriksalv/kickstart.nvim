adcb21ee7d5c1c4ad20f2dcedd26afa3c9c897af
Erik Salvesen <eriksalvesen01@gmail.com>
2025-08-12 22:06:30 +0200

    hopchar2, search marks telescope

--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -8,6 +8,8 @@ return {
     require('hop').setup(opts)
 
     -- Use Hop to jump to a word
-    vim.keymap.set('', '<leader>j', '<cmd>HopPattern<cr>', { desc = 'Hop to pattern' })
+    vim.keymap.set('n', '<leader>j', '<cmd>HopPattern<cr>', { desc = 'Hop to pattern' })
+
+    vim.keymap.set('n', 'zk', '<cmd>HopChar2<cr>', { desc = 'Hop based on 2 chars' })
   end,
 }
